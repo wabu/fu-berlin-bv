c791fc2a1471b079ae714a1ab1519f618a925aca
wabu <waeber@inf.fu-berlin.de>
2010-05-05 00:36:23 +0200

    replaced 0 with samll number to avoid /0 in deconv

--- func/conv.m
+++ func/conv.m
@@ -1,5 +1,13 @@
 function Y = conv(K, I) 
     K_ = fft2d(K);
+    n = length(I);
+    for x=1:n
+        for y=1:n
+            if abs(K_(x,y)) <= 0.01;
+                K_(x,y) = 0.01;
+            end
+        end
+    end
     I_ = fft2d(I);
     Y_ = K_.*I_;
     Y = ffc2d(Y_)/length(I)**2;
--- func/deconv.m
+++ func/deconv.m
@@ -1,5 +1,13 @@
 function Y = deconv(K, I) 
     K_ = fft2d(K);
+    n = length(I);
+    for x=1:n
+        for y=1:n
+            if abs(K_(x,y)) <= 0.01;
+                K_(x,y) = 0.01;
+            end
+        end
+    end
     I_ = fft2d(I);
     Y_ = I_./K_;
     Y = ffc2d(Y_)/length(I)**2;
