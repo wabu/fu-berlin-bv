45cc6c200e6767c6eecce8c390af0e47a4b60579
wabu <waeber@inf.fu-berlin.de>
2010-05-05 00:36:50 +0200

    put all pics into one

--- u03/u03t1.m
+++ u03/u03t1.m
@@ -13,7 +13,7 @@ end
 m = rows/2;
 K = zeros(rows);
 
-R = normrnd(0,20,rows);
+R = normrnd(0,1,rows);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % create kernels
 % 1) Gauss - Sigma 3
@@ -21,26 +21,18 @@ G_s3 = gaussmatrix(rows,3);
 G_s3 = shift(shift(G_s3,-m),-m,2);
 
 If = conv(G_s3, double(I));
-imwrite(uint8(abs(real(If))), 'u03/conv_gauss_3.png');
-
-Id = deconv(G_s3, real(If));
-imwrite(uint8(abs(real(Id))), 'u03/deconv_gauss_3.png');
-
-Id = deconv(G_s3, real(If+R));
-imwrite(uint8(abs(real(Id))), 'u03/deconvr_gauss_3.png');
+Id = deconv(G_s3, (If));
+Id_ = deconv(G_s3, (If+R));
+imwrite(uint8(abs(real([complex(I),Id;If,Id_]))), 'u03/gauss_3.png');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 2) Gauss - Sigma 10
 G_s10 = gaussmatrix(rows,10);
 G_s10 = shift(shift(G_s10,-m),-m,2);
 
 If = conv(G_s10, double(I));
-imwrite(uint8(abs(real(If))), 'u03/conv_gauss_10.png');
-
-Id = deconv(G_s10, real(If));
-imwrite(uint8(abs(real(Id))), 'u03/deconv_gauss_10.png');
-
-Id = deconv(G_s10, real(If+R));
-imwrite(uint8(abs(real(Id))), 'u03/deconvr_gauss_10.png');
+Id = deconv(G_s10, (If));
+Id_ = deconv(G_s10, (If+R));
+imwrite(uint8(abs(real([complex(I),Id;If,Id_]))), 'u03/gauss_10.png');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 3) Sobel_x
 Sobel_x = [1 0 -1; 
@@ -50,24 +42,16 @@ K(1:3, 1:3) = Sobel_x;
 K_sx = shift(shift(K,-1),-1,2);
 
 If = conv(K_sx, double(I));
-imwrite(uint8(abs(real(If))), 'u03/conv_sobel_x.png');
-
-Id = deconv(K_sx, real(If));
-imwrite(uint8(abs(real(Id))), 'u03/deconv_sobel_x.png');
-
-Id = deconv(K_sx, real(If+R));
-imwrite(uint8(abs(real(Id))), 'u03/deconvr_sobel_x.png');
+Id = deconv(K_sx, (If));
+Id_ = deconv(K_sx, (If+R));
+imwrite(uint8(abs(real([complex(I),Id;If,Id_]))), 'u03/sobel_x.png');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 4) Sobel_y
 K_sy = -1*K_sx';
 If = conv(K_sy, double(I));
-imwrite(uint8(abs(real(If))), 'u03/conv_sobel_y.png');
-
-Id = deconv(K_sy, real(If));
-imwrite(uint8(abs(real(Id))), 'u03/deconv_sobel_y.png');
-
-Id = deconv(K_sy, real(If+R));
-imwrite(uint8(abs(real(Id))), 'u03/deconvr_sobel_y.png');
+Id = deconv(K_sy, (If));
+Id_ = deconv(K_sy, (If+R));
+imwrite(uint8(abs(real([complex(I),Id;If,Id_]))), 'u03/sobel_y.png');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 5) uniform (5x5)
 K_uni = zeros(rows);
@@ -75,13 +59,9 @@ K_uni(1:5,1:5) = 0.04;
 K_uni = shift(shift(K_uni,-2), -2,2);
 
 If = conv(K_uni, double(I));
-imwrite(uint8(abs(real(If))), 'u03/conv_uni.png');
-
-Id = deconv(K_uni, real(If));
-imwrite(uint8(abs(real(Id))), 'u03/deconv_uni.png');
-
-Id = deconv(K_uni, real(If+R));
-imwrite(uint8(abs(real(Id))), 'u03/deconvr_uni.png');
+Id = deconv(K_uni, (If));
+Id_ = deconv(K_uni, (If+R));
+imwrite(uint8(abs(real([complex(I),Id;If,Id_]))), 'u03/uni.png');
 
 % If = conv(K, double(I));
 
